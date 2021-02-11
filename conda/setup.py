3259c91881fbc52c009444e73e96023fdc4a8711
Danil <zilov@scamt-itmo.ru>
2021-02-12 01:32:33 +0300

    Create setup.py

---
+++ conda/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages, Extension
+
+setup(
+    name="v2trim",
+    version='0.2.1',
+    description='some description',
+    platforms=["any"],
+    packages=find_packages()
+    )
